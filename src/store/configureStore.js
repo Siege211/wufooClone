5003a59c1a2ad5895e1c22405ab327d0f5646c28
Garrett Eklof <geklof@hardindd.com>
2019-04-22 11:32:19 -0500

    moved initial state object to it's own file

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,25 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
-const initialState = { 
-      fields: [
-        {
-          type: "textSingle"
-        },
-        {
-          type: "textParagraph"
-        },
-        {
-          type: "multipleChoice",
-          choices: []
-        },
-        {
-          type: "checkBoxes",
-          choices: []
-        }
-      ] 
-    };;
-
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
