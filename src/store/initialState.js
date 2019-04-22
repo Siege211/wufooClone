1641a9b6857dd881a96717841d7852f17d137bbb
Garrett Eklof <geklof@hardindd.com>
2019-04-22 11:32:31 -0500

    moved initial state object to it's own file

---
+++ src/store/initialState.js
@@ -0,0 +1,21 @@
+const initialState = { 
+      fields: [
+        {
+          type: "textSingle"
+        },
+        {
+          type: "textParagraph"
+        },
+        {
+          type: "multipleChoice",
+          choices: []
+        },
+        {
+          type: "checkBoxes",
+          choices: []
+        }
+      ] 
+    };
+
+
+export default initialState
