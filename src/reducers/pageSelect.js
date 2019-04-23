d34c451277025d8f021a4ef9e6af77951a817c12
Garrett Eklof <geklof@hardindd.com>
2019-04-23 13:22:01 -0500

    in case PAGE_SELECT, reducer returns action.page instead of an object. This fixed an issue in which state.page was being coerced from a string into an object whenver this reducer was invoked

--- src/reducers/pageSelect.js
+++ src/reducers/pageSelect.js
@@ -1,10 +1,10 @@
 import initialState from '../store/initialState.js';
 
 export default function pageSelect(state = initialState.page, action) {
+	
 	switch(action.type) {
 		case "PAGE_SELECT":
-		console.log(action);
-			return Object.assign({},{page: action.page});
+			return action.page
 		default: 
 			return state
 	}
