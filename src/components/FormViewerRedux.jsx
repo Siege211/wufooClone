2f95379f911c3daa698461ddbb5930b64149997e
Garrett Eklof <geklof@hardindd.com>
2019-04-24 10:30:12 -0500

    removed an unused style object

--- src/components/FormViewerRedux.jsx
+++ src/components/FormViewerRedux.jsx
@@ -8,9 +8,7 @@ const formStyle = {
 	padding: "5px",
 	border: "blue 1px solid"
 }
-const fieldStyle = {
-	border: "1px solid black"
-}
+
 
 const FormViewerRedux = (props) => { 
 	let iterable = props.fields.fields?props.fields.fields:props.fields;
@@ -24,7 +22,6 @@ const FormViewerRedux = (props) => {
 			 (field,i) => {
 			 		return (
 			 			<Field 
-			 				style={fieldStyle}
 			 				key={`field#${i}`} 
 			 				inputType={field.type}
 			 				FIELD_SELECT={props.FIELD_SELECT}
