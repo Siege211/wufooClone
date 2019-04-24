dfd57fb0c9b1096ae638f7b2af99003aa33f9058
Garrett Eklof <geklof@hardindd.com>
2019-04-24 15:28:55 -0500

    case ADD_FIELD returns an array

--- src/reducers/fields.js
+++ src/reducers/fields.js
@@ -4,13 +4,13 @@ import initialState from '../store/initialState.js';
 
 
 export default function updateFieldsList(state = initialState.fields,action) {
+  console.log(state)
+  console.log(action)
   switch(action.type) {
     
     case "ADD_FIELD":
-      return Object.assign({},{
-        fields: Array.isArray(state)?state.concat({type:action.fieldType}):state.fields.concat({type:action.fieldType})
-      });
-    
+      return [...state, action]
+        //fields: Array.isArray(state)?state.concat({type:action.fieldType}):state.fields.concat({type:action.fieldType})
     case "REMOVE_FIELD":
       return Object.assign({},{fields:Array.isArray(state)?state.filter((_, i) => i !== action.fieldIndex):state.fields.filter((_, i) => i !== action.fieldIndex)});
     
