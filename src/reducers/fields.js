7b2fcd705bfee201a5ce421ebdc06c6ec7af42d5
Garrett Eklof <geklof@hardindd.com>
2019-04-25 09:07:12 -0500

    case REMOVE_FIELD now always returns a filtered array

--- src/reducers/fields.js
+++ src/reducers/fields.js
@@ -12,7 +12,11 @@ export default function updateFieldsList(state = initialState.fields,action) {
       return [...state, action]
         //fields: Array.isArray(state)?state.concat({type:action.fieldType}):state.fields.concat({type:action.fieldType})
     case "REMOVE_FIELD":
-      return Object.assign({},{fields:Array.isArray(state)?state.filter((_, i) => i !== action.fieldIndex):state.fields.filter((_, i) => i !== action.fieldIndex)});
+      return state.filter((_, i) => i !== action.fieldIndex);
+
+        // Object.assign({},{fields:Array.isArray(state)?
+        //   state.filter((_, i) => i !== action.fieldIndex)
+        //   :state.fields.filter((_, i) => i !== action.fieldIndex)});
     
     default: 
       return state;
