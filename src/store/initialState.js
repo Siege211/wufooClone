bae206b8bba82e5888bf2580516b757ff00b9a5b
Garrett Eklof <geklof@hardindd.com>
2019-04-25 14:28:14 -0500

    state initializes with no fields in state.fields array

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -3,30 +3,7 @@ import Field_Object from '../fieldObject.js';
 const initialState = { 
       page: "Add a Field",
       selectedField: null,
-      fields: [
-        {
-          fieldLabel: "",
-          type: "textSingle"
-        },
-        {
-          fieldLabel: "",
-          type: "textSingle"
-        },
-        {
-          fieldLabel: "",
-          type: "textParagraph"
-        },
-        {
-          fieldLabel: "",
-          type: "multipleChoice",
-          choices: []
-        },
-        {
-          fieldLabel: "",
-          type: "checkBoxes",
-          choices: []
-        }
-      ] 
+      fields: [] 
     };
 
 
