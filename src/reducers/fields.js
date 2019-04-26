779049149fa836573c68b7aebb5bbbe20abdb854
Garrett Eklof <geklof@hardindd.com>
2019-04-26 14:12:10 -0500

    Added comments for case MODIFY_FIELD

--- src/reducers/fields.js
+++ src/reducers/fields.js
@@ -8,14 +8,15 @@ export default function updateFieldsList(state = initialState.fields,action) {
     
     case "ADD_FIELD":
       return [...state, new Field_Object(action.fieldType)]
-        //fields: Array.isArray(state)?state.concat({type:action.fieldType}):state.fields.concat({type:action.fieldType})
+        
     case "REMOVE_FIELD":
       return state.filter((_, i) => i !== action.fieldIndex);
 
-        // Object.assign({},{fields:Array.isArray(state)?
-        //   state.filter((_, i) => i !== action.fieldIndex)
-        //   :state.fields.filter((_, i) => i !== action.fieldIndex)});
-    
+    case "MODIFY_FIELD":
+      // there is already a selected field
+      // so the index isn't what is needed here
+      // the change to that index is the important information
+      return state
     default: 
       return state;
   }
